d4657af54118aa3afa9594249461479fde1c41e4
mrtnstolk <Martin1@Martins-MacBook-Pro.local>
2015-03-03 13:44:01 +0200

    Dots added

--- main.lua
+++ main.lua
@@ -11,7 +11,7 @@ display.setStatusBar( display.HiddenStatusBar )
 local composer = require "composer"
 
 -- load scene1
-composer.gotoScene( "rangskik" )
+composer.gotoScene( "scene1" )
 
 -- Add any objects that should appear on all scenes below (e.g. tab bar, hud, etc)
 
